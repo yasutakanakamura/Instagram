83d10029f77c40c736a622a8d1994b0dab9b507b
Yasutaka Nakamura <yasutakan@gmail.com>
2018-03-20 01:32:36 +0900

    wip

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,87 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 中村 泰貴 on 2018/03/17.
+//  Copyright © 2018年 yasutaka.nakamura. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var mailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    //ログインボタンをタップした時に呼ばれるメソッド
+    @IBAction func handleLoginButton(_ sender: Any) {
+
+    }
+    
+    //アカウント作成ボタンをタップした時に呼ばれるメソッド
+    @IBAction func handleCreateAcountButton(_ sender: Any) {
+        
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+            
+            
+            //アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty || displayName.isEmpty {
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+            
+            //アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
+            Auth.auth().createUser(withEmail: address, password: password) { user, error in
+                if let error = error {
+                    //エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました。")
+                
+                //表示名を設定する
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error{
+                            print("DEbUG_PRINT: " + error.localizedDescription)
+                        }
+                        print("DEBUG_PRINT: [displayName = \(String(describing:user.displayName))]の設定に成功しました。")
+                        //画面を閉じてViewControllerに戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                } else {
+                    print("DEBUG_PRINT: displayNameの設定に失敗しました。")
+                }
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
